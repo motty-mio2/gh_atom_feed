a5c1e82fd1b806690ba5a66ab5e2041ef7d7644b
motty <motty.mio2@gmail.com>
2025-07-23 22:48:11 +0900

    色々アプデ

--- src/gh_atom_feed/sender.py
+++ src/gh_atom_feed/sender.py
@@ -1,13 +1,27 @@
 import json
+from typing import Any
 
 import requests
 
 
-def send_discord(url: str, message: str) -> bool:
+def send_discord(
+    url: str, content: str | None = None, embeds: dict[str, Any] | None = None
+) -> bool:
+    if content is None and embeds is None:
+        return False
+
+    data: dict[str, Any] = {}
+
+    if content:
+        data["content"] = content
+
+    if embeds:
+        data["embeds"] = embeds
+
     response = requests.post(
         url,
         headers={"Content-Type": "application/json"},
-        data=json.dumps({"content": message}),
+        data=json.dumps(data),
     )
 
     return response.status_code == 204
